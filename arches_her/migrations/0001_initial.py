a3b8f9c7e73151d0d64b1b400ab6bf861989e428
Cyrus Hiatt <chiatt@fargeo.com>
2024-09-24 05:43:00 -0700

    Update arches_her/migrations/0001_initial.py
    Co-authored-by: Andy Jones <andrew.jones@historicengland.org.uk>

--- arches_her/migrations/0001_initial.py
+++ arches_her/migrations/0001_initial.py
@@ -550,11 +550,13 @@ class Migration(migrations.Migration):
 
 
     add_map_source = """
-        INSERT INTO map_sources(name, source)
-            VALUES ('select-application-area', '{
-                "data": "/geojson?nodeid=1909956f-3a3b-11eb-ae99-f875a44e0e11&include_geojson_link=true",
-                "type": "geojson"
-            }');
+INSERT INTO map_sources(name, source)
+    VALUES ('select-application-area', '{
+        "data": "/geojson?nodeid=1909956f-3a3b-11eb-ae99-f875a44e0e11", 
+        "type": "vector", 
+        "tiles": ["/application-areas/{z}/{x}/{y}.pbf"], 
+        "minzoom": 6
+    }');
         """
 
     remove_map_source = """
